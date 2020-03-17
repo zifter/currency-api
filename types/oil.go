ceeec73380d9fc77bf0e553a097343bd73ddec03
Oleg Strokachuk <zifter.ai@gmail.com>
2020-03-17 20:34:19 +0300

    fix backward

--- types/oil.go
+++ types/oil.go
@@ -6,5 +6,6 @@ type OilResponse struct {
 	Value       float64   `json:"value"`
 	Diff        float64   `json:"diff"`
 	DiffPercent float64   `json:"diff_percent"`
-	Timestamp   time.Time `json:"timestamp"`
+	Legacy      string `json:"timestamp"`
+	Timestamp  time.Time `json:"timestamp2"`
 }
